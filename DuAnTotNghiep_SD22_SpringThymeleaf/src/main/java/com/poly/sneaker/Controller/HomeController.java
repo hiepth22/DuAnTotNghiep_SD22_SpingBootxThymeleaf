8ff1e31ee790325c04fafe1dc3d0bd4f9708a09f
hiepth22 <106100259+hiepth22@users.noreply.github.com>
2024-06-18 07:24:07 +0700

    Tao du an spring x thymeleaf

---
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/Controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.poly.sneaker.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/home")
+    public String index() {
+        return "admin/index";
+    }
+}
