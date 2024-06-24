4f7f5a41ad71ccba60e7a4926bb36aaa6e35fc09
TUANLTPH30142 <letientuan0107@gmail.com>
2024-06-24 12:54:46 +0700

    NhanVien

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/TaiKhoan/NhanVien/NhanVienControler.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/TaiKhoan/NhanVien/NhanVienControler.java
@@ -11,12 +11,18 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @Controller
+@RequestMapping("/admin")
 public class NhanVienControler {
      @Autowired
     private NhanVienSevice sevice;
 
+    @GetMapping("/nhanvien")
+    public String HienThinv(){
+        return "admin/NhanVien/NhanVienIndext";
+    }
 
      @GetMapping("/nhan-vien")
     public String HienThi(Model model){
@@ -25,7 +31,6 @@ public class NhanVienControler {
      return "admin/NhanVien/NhanVien";
     }
 
-<<<<<<< HEAD
     @GetMapping("/addNhanVien")
     public String NhanVienadd(Model model){
         NhanVien nv = new NhanVien();
@@ -43,7 +48,7 @@ public class NhanVienControler {
         nv.setNgaytao(java.time.LocalDateTime.now());
         nv.setNgaycapnhap(java.time.LocalDateTime.now());
         sevice.Add(nv);
-        return "redirect:/nhan-vien";
+        return "redirect:/admin/nhan-vien";
     }
     @GetMapping("/UpdateNhanVien/{id}") //
     public String showEmployeeDetail(@PathVariable("id") Long id, Model model) {
@@ -51,11 +56,28 @@ public class NhanVienControler {
         model.addAttribute("nv", employee);
         return "admin/NhanVien/NhanVienUpdate";
     }
-//    @PutMapping("/UpdateNhanVien")
-//    public String Update(@Valid @ModelAttribute("nhanVien") NhanVien nv, BindingResult result, Model model){
-//         sevice.update()
-//    }
+    @PostMapping("/updateNhanVien/{id}")
+    public String updateNhanVien(@PathVariable("id") Long id, @Valid @ModelAttribute("nv") NhanVien nv, BindingResult result) {
+        if (result.hasErrors()) {
+            return "admin/NhanVien/NhanVienUpdate";
+        }
 
-=======
->>>>>>> a8b4d429b5a4450775e97f687c18dc17e16635b7
+        NhanVien updatedNv = sevice.update(id, nv);
+        if (updatedNv != null) {
+            return "redirect:/admin/nhan-vien";
+        } else {
+            return "redirect:/admin/nhan-vien";
+        }
+    }
+    @GetMapping("/DetailNhanVien/{id}")
+    public String chiTietNhanVien(@PathVariable("id") Long id, Model model) {
+        Optional<NhanVien> optionalNhanVien = Optional.ofNullable(sevice.findById(id));
+        if (optionalNhanVien.isPresent()) {
+            model.addAttribute("nv", optionalNhanVien.get());
+            return "admin/NhanVien/NhanVienDetail";
+        } else {
+            return "redirect:/admin/nhan-vien";
+        }
+    }
+
 }
