4cbe01c0377dfa8d3096deea2b7c18a4a83c7db7
dangnb <dangnb163@gmail.com>
2024-06-25 14:21:53 +0700

    add hoa don detail

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/HoaDonController.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/HoaDonController.java
@@ -1,17 +1,22 @@
 package com.poly.sneaker.controller;
 
 import com.poly.sneaker.entity.HoaDon;
+import com.poly.sneaker.entity.HoaDonChiTiet;
+import com.poly.sneaker.entity.NhanVien;
+import com.poly.sneaker.sevice.HoaDonChiTietService;
 import com.poly.sneaker.sevice.HoaDonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
+
 
 @Controller
 public class HoaDonController {
@@ -19,19 +24,50 @@ public class HoaDonController {
     @Autowired
     HoaDonService hoaDonService;
 
-//    @GetMapping("/hoa-don")
-//    public ResponseEntity<List<HoaDon>> getAllHoaDon() {
-//        List<HoaDon> hoaDons = hoaDonService.getAll();
-//        return ResponseEntity.ok(hoaDons);
-//    }
+    @Autowired
+    HoaDonChiTietService hoaDonChiTietService;
 
-        @GetMapping("/hoa-don")
-    public String hienThiHoaDon(@RequestParam(name = "tab", required = false, defaultValue = "0") int tab, Model model) {
-        List<HoaDon> hoaDons = hoaDonService.getAllbyTrangThai(tab);
+    @GetMapping("/hoa-don")
+    public String hienThiHoaDon(@RequestParam(name = "tab", required = false, defaultValue = "0") int tab,
+                                @RequestParam(name = "ma", required = false) String ma,
+                                @RequestParam(name = "startDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate,
+                                @RequestParam(name = "endDate", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
+                                Model model) {
+        List<HoaDon> hoaDons;
+
+        if (ma != null && startDate != null && endDate != null) {
+            hoaDons = hoaDonService.findHoaDonByMaAndNgayTao(ma, startDate, endDate);
+        } else {
+            hoaDons = hoaDonService.getAllbyTrangThai(tab);
+        }
+
         model.addAttribute("hoaDons", hoaDons);
         model.addAttribute("currentTab", tab);
-            System.out.println();
-        return "admin/HoaDon";
+        return "admin/HoaDon/HoaDon";
+    }
+
+//    @GetMapping("/hoa-don/detail/{id}") //
+//    public String detail(@PathVariable("id") Long id, Model model) {
+//        HoaDon hd = hoaDonService.detail(id);
+//        HoaDonChiTiet hdct = hoaDonChiTietService.detail(id);
+//        BigDecimal tongTien = hoaDonChiTietService.tongTien(id);
+//
+//        System.out.println(hdct);
+//        model.addAttribute("hd", hd);
+//        model.addAttribute("hdct", hdct);
+//        model.addAttribute("tongTien", tongTien);
+//        return "admin/HoaDon/HoaDonDetail";
+//    }
+
+    @GetMapping("/hoa-don/detail/{id}")
+    public String showHoaDonDetail(@PathVariable("id") Long id, Model model) {
+        HoaDon hd = hoaDonService.detail(id);
+        List<HoaDonChiTiet> hdctList = hoaDonChiTietService.findByHDId(id);
+
+
+        model.addAttribute("hd", hd);
+        model.addAttribute("hdctList", hdctList);
+        return "admin/HoaDon/HoaDonDetail";
     }
 
 
--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/sevice/HoaDonService.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/sevice/HoaDonService.java
@@ -1,17 +1,25 @@
 package com.poly.sneaker.sevice;
 
 import com.poly.sneaker.entity.HoaDon;
+import com.poly.sneaker.entity.HoaDonChiTiet;
+import com.poly.sneaker.entity.NhanVien;
+import com.poly.sneaker.repository.HoaDonChiTietRepository;
 import com.poly.sneaker.repository.HoaDonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class HoaDonService {
+
     @Autowired
     private HoaDonRepository hoaDonRepository;
+
+    @Autowired
+    private HoaDonChiTietRepository hoaDonChiTietRepository;
 
 
     public List<HoaDon> getAll(){
@@ -24,12 +32,24 @@ public class HoaDonService {
         } else {
             return hoaDonRepository.findAllbyTrangThai(trangThai);
         }
+    }
 
+    public List<HoaDon> findHoaDonByMaAndNgayTaoAndTrangThai(String ma, Date startDate, Date endDate, int trangThai) {
+        return hoaDonRepository.findByMaAndNgayTaoBetweenAndTrangThai(ma, startDate, endDate, trangThai);
+    }
+
+    public List<HoaDon> findHoaDonByMaAndNgayTao(String ma, Date startDate, Date endDate) {
+        return hoaDonRepository.findByMaAndNgayTaoBetween(ma, startDate, endDate);
     }
 
 
     public HoaDon add(HoaDon hoaDon) {
         return hoaDonRepository.save(hoaDon);
+    }
+
+    public HoaDon detail(Long id) {
+        Optional<HoaDon> optional = hoaDonRepository.findById(id);
+        return optional.map(o -> o).orElse(null);
     }
 
 
