c83933cf49115d8e61a758835572a56f21410795
hiepth22 <106100259+hiepth22@users.noreply.github.com>
2024-06-28 11:45:47 +0700

    update giao dien

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/resources/static/assets/js/sptchAdd.js
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/resources/static/assets/js/sptchAdd.js
@@ -9,11 +9,3 @@ function selectColors() {
     });
     $('#colorModal').modal('hide'); // Đóng modal sau khi chọn
 }
-$(document).ready(function(){
-    var multipleCancelButton = new Choices('#choices-multiple-remove-button', {
-        removeItemButton: true,
-        maxItemCount:5,
-        searchResultLimit:5,
-        renderChoiceLimit:5
-    });
-});
