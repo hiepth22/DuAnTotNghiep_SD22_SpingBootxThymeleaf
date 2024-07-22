541e71e91f8e119d6d5a4fef9e3435af96da519b
dangnb <dangnb163@gmail.com>
2024-07-22 18:40:41 +0700

    update hoadon

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/sevice/PhuongThucThanhToanService.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/sevice/PhuongThucThanhToanService.java
@@ -1,6 +1,7 @@
 package com.poly.sneaker.sevice;
 
 import com.poly.sneaker.entity.HoaDon;
+import com.poly.sneaker.entity.LichSuHoaDon;
 import com.poly.sneaker.entity.PhuongThucThanhToan;
 import com.poly.sneaker.repository.PhuongThucThanhToanRepository;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,14 @@ public class PhuongThucThanhToanService {
     public PhuongThucThanhToan detail(Long id) {
         Optional<PhuongThucThanhToan> optional = phuongThucThanhToanRepository.findByIdHoaDon(id);
         return optional.map(o -> o).orElse(null);
+    }
+
+    public PhuongThucThanhToan updateTrangThaiThanhToan(Long id, PhuongThucThanhToan phuongThucThanhToan) {
+        Optional<PhuongThucThanhToan> optional = phuongThucThanhToanRepository.findById(id);
+        return optional.map(o -> {
+            o.setLoaiThanhToan(phuongThucThanhToan.getLoaiThanhToan());
+            return phuongThucThanhToanRepository.save(o);
+        }).orElse(null);
     }
 
 }
