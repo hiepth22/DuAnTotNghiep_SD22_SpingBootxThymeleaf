b07b1c6f68d42a7cf256f69ada38acd6e7b1855b
TUANLTPH30142 <letientuan0107@gmail.com>
2024-07-23 15:27:31 +0700

    dky khách Hàng de lay tk mk oke,con lay lai mk,với mã hóa mật khẩu,chưa validate

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/login/login.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/controller/login/login.java
@@ -2,10 +2,12 @@ package com.poly.sneaker.controller.login;
 
 import com.poly.sneaker.entity.KhachHang;
 import com.poly.sneaker.repository.KhachHangRepository;
+import com.poly.sneaker.sevice.KhachHangService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
@@ -14,6 +16,8 @@ public class login {
 
     @Autowired
     private KhachHangRepository khachHangRepository;
+    @Autowired
+    private KhachHangService service;
 
     @GetMapping("/login")
     public String showLoginForm(Model model) {
@@ -34,6 +38,42 @@ public class login {
             }
         }
         model.addAttribute("error", true);
+        return "Login/login";
+    }
+
+    @PostMapping("/loginadd")
+    public String loginadd(@RequestParam(name = "ten") String ten,
+                           @RequestParam(name = "email") String email,
+                           @RequestParam(name = "matKhau") String matKhau,
+                           @RequestParam(name = "matKhaulai") String matKhaulai,
+                           Model model) {
+        if (matKhau.equals(matKhaulai)) {
+            KhachHang kh = new KhachHang();
+            kh.setTen(ten);
+            kh.setEmail(email);
+            kh.setMatKhau(matKhau);
+            kh.setTrangThai(1);
+            kh.setNgaytao(java.time.LocalDateTime.now());
+            kh.setNgaycapnhap(java.time.LocalDateTime.now());
+            service.Add(kh);
+            return "Login/login";
+        }
+        else {
+            return "Login/login";
+        }
+//        model.addAttribute("error", true);
+//        return "Login/login";
+    }
+    @PostMapping("/laylaimk")
+    public String laylaimk(@RequestParam(name = "email") String email,
+                           @RequestParam(name = "ma") String ma,
+                           Model model) {
+        return "Login/login";
+    }
+    @PostMapping("/laylaima")
+    public String laylaima(@RequestParam(name = "email") String email,
+                           @RequestParam(name = "ma") String ma,
+                           Model model) {
         return "Login/login";
     }
 }
