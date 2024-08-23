9504dd8236f109befba63c0b069b03317c5adcd2
TUANLTPH30142 <letientuan0107@gmail.com>
2024-08-23 19:00:34 +0700

    xổ xuống của đăng đây

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/resources/static/assets/js/Header.js
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/resources/static/assets/js/Header.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
         method: 'GET',
         success: function (response) {
             const checklogin = $('#checklogin');
+            const nguoidung = $('#nguoidung');
+
             let idKhachHang = response.id;
 
 
@@ -11,6 +13,15 @@ $(document).ready(function () {
 
                                <p class="card-title clickable" style="color: orange; margin: 10px;">Thông Tin (ID Người Dùng: ${idKhachHang})</p>`;
             checklogin.html(content);
+            let nguoi = `<a  class="border-0">
+                            <i class="bi bi-person" style="color: white; font-weight: bold;"></i>
+                        </a>
+                        <div class="dropdown-menu">
+                           <a  class="dropdown-item">Thông Tin Cá Nhân</a>
+                            <a  class="dropdown-item">Đăng Xuất</a>
+                        </div>
+                       `;
+            nguoidung.html(nguoi);
 
             $('.clickable').on('click', function () {
                 let url = $(this).data('url');
@@ -18,10 +29,22 @@ $(document).ready(function () {
             });
         },
         error: function (xhr, status, error) {
+            const nguoidung = $('#nguoidung');
             const checklogin = $('#checklogin');
             checklogin.html(`
-                <a class="card-title clickable" style="color: orange; margin: 10px;" data-url="http://localhost:3000/thong-tin-don-hang">Đơn Hàng</a>
+                <a  class="card-title clickable" style="color: orange; margin: 10px;" data-url="http://localhost:3000/thong-tin-don-hang">Đơn Hàng</a>
                 <a class="card-title clickable" style="color: orange; margin: 10px;" data-url="http://localhost:3000/login">Đăng nhập</a>
+                 `);
+            nguoidung.html(`
+              
+                        <a  class="border-0">
+                            <i class="bi bi-person" style="color: white; font-weight: bold;"></i>
+                        </a>
+                        <div class="dropdown-menu">
+                           
+                            <a href="" class="dropdown-item">login</a>
+                        </div>
+                   
                  `);
 
             $('.clickable').on('click', function (event) {
