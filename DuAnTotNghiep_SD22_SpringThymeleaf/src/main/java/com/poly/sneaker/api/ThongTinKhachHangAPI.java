aa803a363023f9030952efacc08c47ec7171b1cb
dangnb <dangnb163@gmail.com>
2024-09-12 19:20:09 +0700

    -------------------------------------------------------

--- DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/api/ThongTinKhachHangAPI.java
+++ DuAnTotNghiep_SD22_SpringThymeleaf/src/main/java/com/poly/sneaker/api/ThongTinKhachHangAPI.java
@@ -26,7 +26,7 @@ public class ThongTinKhachHangAPI {
     public ResponseEntity<?> thongTinHoaDon(@PathVariable("id") Long id) {
         return ResponseEntity.ok(hoaDonService.findByIDKHAndTrangThai(id));
     }
-  // 22222s
+  // 22222ss
     @GetMapping("/thong-tin-hoa-don-chi-tiet/{idHoaDon}")
     public ResponseEntity<List<HoaDonChiTiet>> getHoaDonChiTiet(
             @PathVariable Long idHoaDon,
